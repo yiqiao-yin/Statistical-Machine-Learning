c069aa91ee5c4eb926db222438c99dc6974f01c2
Yiqiao Yin <32333117+yiqiao-yin@users.noreply.github.com>
2019-03-19 00:11:58 -0400

    Add files via upload

---
+++ Exercise/Exercise-MC-Simulation-Pi.R
@@ -0,0 +1,16 @@
+################### MONTE CARLO SIMULATION OF PI #########################
+
+## Define a function to output a plot of pi
+piR.plot <- function(N) {
+  x <- runif(N)
+  y <- runif(N)
+  d <- sqrt(x^2 + y^2)
+  label <- ifelse(d < 1, 1, 0)
+  plot(x,y,col=label+1,main=paste0("Simulation of Pi Using N=",N))
+}
+
+## Plot Monte Carlo Simulation of Pi
+saveGIF({
+  for (i in c(100,200,500,1000,2000,5000,10000,15000,20000)) {piR.plot(i)}
+}, movie.name = "brownian_motion.gif", interval = 0.3, nmax = 30, 
+ani.width = 500)
